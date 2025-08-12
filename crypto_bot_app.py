692a2e30fbf9ebf3f594da055bf6068320bdcc3e
salahudeenb <salahudeen123p@gmail.com>
2025-08-11 18:23:33 -0600

    Update crypto_bot_app.py

--- crypto_bot_app.py
+++ crypto_bot_app.py
@@ -1,39 +1,3 @@
-import os
-import streamlit as st
-import pandas as pd
-import ccxt
-import numpy as np
-from datetime import datetime
-from dotenv import load_dotenv
-import plotly.graph_objects as go
-
-fig = go.Figure()
-
-
-fig.add_trace(go.Candlestick(
-    x=df['timestamp'],
-    open=df['open'],
-    high=df['high'],
-    low=df['low'],
-    close=df['close'],
-    name='OHLC'
-))
-
-fig.add_trace(go.Scatter(
-    x=df['timestamp'],
-    y=df['ema_fast'],
-    line=dict(color='blue', width=1),
-    name=f'EMA {FAST_EMA}'
-))
-
-fig.add_trace(go.S
-    x=df['timestamp'],
-    y=df['ema_slow'],
-    line=dict(color='orange', width=1),
-    name=f'EMA {SLOW_EMA}'
-))
-
-# Volume as bar chart on a secondary y-axis
 fig.add_trace(go.Bar(
     x=df['timestamp'],
     y=df['volume'],
